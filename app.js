a28fa95af57e85012f937bb6358bf2cb8440c233
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-12-06 17:11:48 +0100

    añado función getallposts

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ require('./dbConfig').createPool();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
 const conceptsRouter = require('./routes/concepts');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
@@ -29,8 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use('/concepts', conceptsRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
