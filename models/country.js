3d87c91b72de306090d50fc1bce77b00336a07a7
Alice López Gómez <alicelop@ucm.es>
2020-12-12 23:21:23 +0100

    limpieza

--- models/country.js
+++ models/country.js
@@ -17,14 +17,11 @@ const getCountriesByCode = (pCodes) => {
 // recupero un pais por nombre 
 const getCountriesByname = (pName) => {
     return new Promise((resolve, reject) => {
-        console.log(pName);
-
         db.query('SELECT * FROM countries WHERE name_es LIKE ? OR name_en LIKE ? OR official_name LIKE ?', ['%' + pName + '%', '%' + pName + '%', '%' + pName + '%'], (error, rows) => {
             if (error) reject(error);
             if (rows.length === 0) resolve(null);
             resolve(rows);
         });
-
     });
 };
 
