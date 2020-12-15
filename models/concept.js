bb514715ae146a64a81b40fb8d4e8f503a1b6524
alicelg <alicelop@ucm.es>
2020-12-15 17:03:46 +0100

    insertar conceptos favoritos

--- models/concept.js
+++ models/concept.js
@@ -33,6 +33,23 @@ const getConceptsByPage = (pPagina) => {
     });
 }
 
+
+const insertFavorite = (favorite) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.SECRET_KEY);
+
+    const idUsuario = user.id;
+    const confavorite = favorite;
+    console.log(confavorite);
+
+    return new Promise((resolve, reject) => {
+        db.query('INSERT INTO favorite_concepts (fk_user, fk_concept) VALUES (?,?)', [idUsuario, confavorite], (error, rows) => {
+            if (error) reject(error);
+            resolve(rows)
+        });
+    });
+}
+
 module.exports = {
-    getAllConcepts, getConceptsByTitle, getConceptsByPage
+    getAllConcepts, getConceptsByTitle, getConceptsByPage, insertFavorite
 }
--- routes/concepts.js
+++ routes/concepts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllConcepts, getConceptsByTitle, getConceptsByPage } = require('../models/concept');
+const { getAllConcepts, getConceptsByTitle, getConceptsByPage, insertFavorite } = require('../models/concept');
 
 /* GetAllConcepts  */
 router.get('/', async (req, res) => {
@@ -36,8 +36,21 @@ router.get('/page/:pPagina', (req, res) => {
     }); */
 })
 
+/* ruta para favoritos conceptos */
+router.post('/favorite', (req, res) => {
+    console.log(req.body);
 
+    insertFavorite(req.body.idconcepto)
+        .then(favorite => {
+            res.json(favorite);
+        }
 
+        )
+    /* .catch(error => {
+        res.status(400).json({ error: process.env.RESPONSE_NOT_FOUND })
+    });
+*/
+})
 
 
 module.exports = router;
