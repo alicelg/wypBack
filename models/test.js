8ef5d753e6871df0917f0ca295ff6a6310d3becf
Alice López Gómez <alicelop@ucm.es>
2020-12-16 22:51:42 +0100

    resultado de test completado

--- models/test.js
+++ models/test.js
@@ -1,13 +1,10 @@
-/* Funciones sobre el post   */
-
-/* GET */
+/* Funciones sobre el test   */
 
 
 // recupero las preguntas del test
-
-const getQuestionsByTest = (pTest) => {
+const getQuestionsByTest = (pTestId) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM questions WHERE test_id = ?', [pTest], (error, rows) => {
+        db.query('SELECT * FROM questions WHERE test_id = ? ORDER BY RAND() LIMIT 10', [pTestId], (error, rows) => {
             if (error) reject(error);
             if (rows.length === 0) resolve(null);
             resolve(rows);
@@ -15,17 +12,73 @@ const getQuestionsByTest = (pTest) => {
     });
 };
 
+
+const getTimesRepeated = (pTestId, pUserId) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT times_repeated FROM test_results WHERE test_id = ? AND user_id = ?', [pTestId, pUserId], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows[0]);
+        });
+    });
+};
+
+
 const setAnswers = (pAnswersArray) => {
     return new Promise((resolve, reject) => {
-        db.query("INSERT INTO user_answers (question_id, answer_id, user_id ) VALUES ?", [pAnswersArray], (error, rows) => {
+        db.query("INSERT INTO user_answers (test_id, question_id, answer_id, user_id, times_repeated ) VALUES ?", [pAnswersArray], (error, rows) => {
             if (error) reject(error);
             if (rows.length === 0) resolve(null);
             resolve(rows);
+        });
+    });
+};
+
+
+const getUserAnswers = (pTestId, pUserId, pTimesRepeated) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM user_answers WHERE test_id = ? AND user_id = ? AND times_repeated = ?', [pTestId, pUserId, pTimesRepeated], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows);
+        });
+    });
+};
+
+
+const getQuestionsAnswers = (pQuestionIdsArray) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT id, answer_valid FROM questions WHERE id IN (?)', [pQuestionIdsArray], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows);
+        });
+    });
+};
+
+
+const setResult = (pTestId, pUserId, pInitDate, pTimesRepeated, pRightAnswers, pTotalAnswers, pPercentResult) => {
+    return new Promise((resolve, reject) => {
+        db.query("INSERT INTO test_results (test_id, user_id, init_date, times_repeated, right_answers, total_answers, percent_result ) VALUES (?,?,?,?,?,?,?)", [pTestId, pUserId, pInitDate, pTimesRepeated, pRightAnswers, pTotalAnswers, pPercentResult], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows);
+        });
+    });
+};
+
+
+const getResult = (pResultId) => {
+    return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM test_results WHERE id = ? ", [pResultId], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows[0]);
         });
     });
 };
 
 
 module.exports = {
-    getQuestionsByTest, setAnswers
+    getQuestionsByTest, getTimesRepeated, setAnswers, getUserAnswers, getQuestionsAnswers, setResult, getResult
 }
--- routes/tests.js
+++ routes/tests.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getQuestionsByTest, setAnswers } = require('../models/test');
+const { getQuestionsByTest, getTimesRepeated, setAnswers, getQuestionsAnswers, setResult, getResult } = require('../models/test');
 const jwt = require('jsonwebtoken');
 
 
@@ -15,25 +15,75 @@ router.get('/:testId/questions', async (req, res) => {
 });
 
 /* setAnswers  */
-router.post('/:testId/answers', async (req, res) => {
+router.post('/answers', async (req, res) => {
     const answersArray = req.body.answersArray;
+    const testId = req.body.testId;
+    const initDate = req.body.initDate;
+
+    // extraemos el usuario del token
     const token = req.headers.authorization.split(" ")[1];
-    const testId = req.params.testId;
-
-
     const user = jwt.verify(token, process.env.SECRET_KEY);
 
+
+    // obtenemos el numero de veces que el usuario ha realizado el test
+    let timesRepeated;
+    try {
+        timesRepeated = await getTimesRepeated(testId, user.id);
+    } catch (error) {
+        res.status(400).json({ error: process.env.RESPONSE_ERROR_ON_SAVE })
+    }
+
+    // si es la primera vez le asignamos 1
+    if (timesRepeated == null) {
+        timesRepeated = 1;
+    } else {
+        timesRepeated = timesRepeated.times_repeated + 1
+    }
+
+    // generamos el array a insertar en base de datos
     answersArray.map(answer => {
-        answer.userId = user.id;
+        answer.push(user.id, timesRepeated);
     })
-    console.log(answersArray);
-    
+
+    // seteamos las respuestas
+    try {
+        await setAnswers(answersArray);
+    } catch (error) {
+        res.status(400).json({ error: process.env.RESPONSE_ERROR_ON_SAVE })
+    }
 
     try {
-        const rows = await setAnswers( answersArray);
-        res.json(rows);
+        // obtenemos los ids de las preguntas respondidas
+        const questionIdsArray = answersArray.map(answer => answer[1])
+
+        // obtenemos las respuestas válidas guardadas en la BBDD
+        const validAnswers = await getQuestionsAnswers(questionIdsArray)
+
+        // calculamos el numero total de preguntas
+        const totalAnswers = answersArray.length
+
+        let rightAnswers = 0
+
+        // recorremos el array de respuestas del usuario y comprobamos si coincide con la respuesta válida
+        answersArray.map(answer => {
+            if (validAnswers.find(validAnswer => validAnswer.id === answer[1]).answer_valid == answer[2]) {
+                rightAnswers++
+            }
+        })
+
+        // calculamos el resultado porcentual
+        const percentResult = rightAnswers / totalAnswers * 100
+
+        // insertamos el resultado en la BBDD
+        const insertResult = await setResult(testId, user.id, initDate, timesRepeated, rightAnswers, totalAnswers, percentResult)
+
+        // obtenemos el resultado insertado
+        const resultData = await getResult(insertResult.insertId)
+
+        res.json(resultData);
+
     } catch (error) {
-        res.status(400).json({ error: process.env.RESPONSE_NOT_FOUND })
+        res.status(400).json({ error: process.env.RESPONSE_ERROR_ON_SAVE })
     }
 });
 
